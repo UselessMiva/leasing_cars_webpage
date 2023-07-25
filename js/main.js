a0288b0f573fe52d8eefc56b7a5113eb7f576e21
H0r0sho <undeaddragon163rus@gmail.com>
2023-07-25 22:23:46 +0400

    1st pade completed

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+      },
+      effect: "fade",
+  });
