6fba8d23585d885d1f04a26d1eb4e32e66b80763
H0r0sho <undeaddragon163rus@gmail.com>
2023-07-29 04:37:19 +0400

    completed xecond and last pages without adaptive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach(function(element) {
+  element.addEventListener('click', open);
+})
+function open(evt){
+  const tabTarger = evt.currentTarget;
+  const button = tabTarger.dataset.button;
+
+tabItem.forEach(function(item){
+  item.classList.remove('tabs__btn-item--active');
+
+  }) 
+
+  tabTarger.classList.add('tabs__btn-item--active');
+
+  tabContent.forEach(function(item){
+    item.classList.remove('tabs__content-item--active')
+  })
+
+  document.querySelector(`#${button}`).classList.add('tabs__content-item--active')}
+
 const swiper = new Swiper(".swiper", {
-    pagination: {
-      el: ".swiper-pagination",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+      delay: 6000,
+      disableOnInteraction: false,
     },
-    autoplay: {
-        delay: 6000,
-        disableOnInteraction: false,
-      },
-      effect: "fade",
-  });
+    effect: "fade",
+});
